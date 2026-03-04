0b058ba15afce53b259d5534603c937415d1a4b6
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2026-03-04 10:28:33 +0100

    refactor: rename Events dir to lowercase

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { glob } from "astro/loaders";
 const events = defineCollection({
 	loader: glob({
 		pattern: "**/*.md",
-		base: "./src/content/Events",
+		base: "./src/content/events",
 	}),
 });
 
